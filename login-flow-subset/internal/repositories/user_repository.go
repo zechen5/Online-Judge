33f3ab407feed3f3d3668f33a5ac0806577c8e52
zechen5 <zechen5@illinois.edu>
2026-04-07 19:11:22 -0500

    Add login flow subset user repository

---
+++ login-flow-subset/internal/repositories/user_repository.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"gorm.io/gorm"
+
+	"online-judge-backend/login-flow-subset/internal/models"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) GetByID(id uint) (*models.User, error) {
+	var user models.User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) GetByUsername(username string) (*models.User, error) {
+	var user models.User
+	if err := r.db.Where("username = ?", username).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
